43cbb0eb426386c24a06f2d2861dfe3295d36ec0
starwalker8p <starwalker8p@gmail.com>
2023-10-16 14:50:31 -0300

    fiz a atividade

---
+++ multa.py
@@ -0,0 +1,5 @@
+velocidade = float(input("velocidade: "))
+if velocidade > 60:
+    print(f"por conta da alta velocidade você tomara uma multa de {velocidade*7:.2f}")
+else: 
+    print("dentro da lei")
